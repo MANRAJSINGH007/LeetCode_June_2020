85f38e33b7a77fb54ead420a427e599e36fb50c9
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2020-06-26 22:01:13 +0530

    Create Sum_Root_to_Leaf_Numbers.java

---
+++ Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int sum = 0;
+    public static void func(TreeNode root, int x){
+        if(root == null) return;
+        if(root.left == null && root.right == null) sum += 10 * x + root.val;
+        func(root.left, x * 10 + root.val);
+        func(root.right, x * 10 + root.val);
+        return ;
+    }
+    public int sumNumbers(TreeNode root) {   
+        sum = 0;
+        func(root, 0);
+        return sum;
+    }
+}
